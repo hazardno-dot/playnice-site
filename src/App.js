806faac0518bc3c8c5adb2f09fe32dff07784325
hazardno-dot <hazardno@gmail.com>
2026-03-22 22:01:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
-git add .
-git commit -m "Debug App.js"
-git push
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center", marginTop: "40px", color: "white" }}>
+        PlayNice
+      </h1>
+      <p style={{ textAlign: "center", color: "white" }}>
+        Debug version is working.
+      </p>
+    </div>
+  );
+}
+
+export default App;
