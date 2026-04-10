6ab9162102667081c12c21eb841d4652cc336b26
hazardno-dot <hazardno@gmail.com>
2026-04-10 20:16:36 +0200

    js

--- src/App.js
+++ src/App.js
@@ -2091,12 +2091,15 @@ useEffect(() => {
 
   if (shouldLockScroll) {
     document.body.style.overflow = "hidden";
+    document.documentElement.style.overflow = "hidden";
   } else {
     document.body.style.overflow = "";
+    document.documentElement.style.overflow = "";
   }
 
   return () => {
     document.body.style.overflow = "";
+    document.documentElement.style.overflow = "";
   };
 }, [
   selectedProduct,
