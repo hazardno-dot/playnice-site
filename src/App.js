615d4f685a40726ca19ecbe6843476f8f50bf050
hazardno-dot <hazardno@gmail.com>
2026-04-12 00:16:08 +0200

    css i js

--- src/App.js
+++ src/App.js
@@ -170,7 +170,7 @@ const translations = {
     // PRODUCT CARD
     tryFrom: "From",
     productDecantNote: "Wear it first. Decide later.",
-    productCardCta: "Start with 5ml 👇",
+    productCardCta: "Start with 5ml →",
 
     sizeBestChoice: "Best pick",
 
@@ -350,7 +350,7 @@ const translations = {
     // PRODUCT CARD
     tryFrom: "Već od",
     productDecantNote: "Nosi ga prvo. Odluči kasnije.",
-    productCardCta: "Kreni sa 5ml 👇",
+    productCardCta: "Kreni sa 5ml →",
 
     sizeBestChoice: "Preporuka",
 
