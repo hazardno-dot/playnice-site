aebbbd0a3eb83b4ef3701acce60ece390c09f99e
hazardno-dot <hazardno@gmail.com>
2026-04-23 20:36:58 +0200

    js

--- src/App.js
+++ src/App.js
@@ -4347,7 +4347,7 @@ const getSizeWearHint = (size) => {
             setSelectedArticle(null);
           }}
         >
-          <span className="header-journal-btn-text">TEST JOURNAL</span>
+          <span className="header-journal-btn-text">Journal</span>
         </button>
       </nav>
 
