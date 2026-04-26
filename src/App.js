3ede0fffaa72fc63cbacb943e73af95415141207
hazardno-dot <hazardno@gmail.com>
2026-04-26 18:35:36 +0200

    js

--- src/App.js
+++ src/App.js
@@ -3757,8 +3757,8 @@ const handleJournalFeedbackSubmit = (article) => {
 
 const latestJournalArticle = journalArticles?.[0] || null;
 
-const latestJournalArticleKey = latestJournalArticle
-  ? getJournalArticleKey(latestJournalArticle)
+const latestJournalArticleKey = latestJournalArticle?.id
+  ? String(latestJournalArticle.id)
   : "";
 
 const hasNewJournalArticle =
@@ -3772,13 +3772,16 @@ const markLatestJournalAsSeen = () => {
 
   try {
     localStorage.setItem(JOURNAL_SEEN_KEY, latestJournalArticleKey);
-  } catch {}
+  } catch (error) {
+    console.error("Failed to save seen journal article:", error);
+  }
 
   setSeenLatestJournalKey(latestJournalArticleKey);
 };
 
 const handleJournalOpen = () => {
   setJournalOpen(true);
+  setSelectedArticle(null);
   markLatestJournalAsSeen();
 };
 
