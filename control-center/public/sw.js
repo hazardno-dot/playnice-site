d562166920c48aa2b27f900a086fcddd1be96811
hazardno-dot <hazardno@gmail.com>
2026-08-29 15:51:15 +0200

    Switch Control Center PWA branding to gold

--- control-center/public/sw.js
+++ control-center/public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "playnice-cc-v2";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/pwa-192.svg", "/pwa-512.svg"];
+const CACHE_NAME = "playnice-cc-v3";
+const APP_SHELL = ["/", "/manifest.webmanifest", "/pwa-192.webp", "/pwa-512.webp", "/pwa-favicon.webp"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
