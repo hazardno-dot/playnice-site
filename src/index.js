7bc52a207f8cc4213970ad6c77d2370741a2cbf3
hazardno-dot <hazardno@gmail.com>
2026-08-31 16:22:51 +0200

    Mount Private Selection personal layer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import PrivateSelectionEnhancer from "./PrivateSelectionEnhancer";
 import "./PrivateSelectionV1.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <PrivateSelectionEnhancer />
   </React.StrictMode>
 );
