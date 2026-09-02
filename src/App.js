719b3accc8dcfe03df07f133197c8781b1c6a658
hazardno-dot <hazardno@gmail.com>
2026-09-02 18:38:31 +0200

    test

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ const normalizeShopSearch = (value = "") =>
 /* =========================================
    SEO helper
 ========================================= */
-
 const SITE_BASE_URL = "https://www.playniceshop.me";
 
 const cleanSeoProductName = (name = "") =>
