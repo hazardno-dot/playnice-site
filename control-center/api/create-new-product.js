160870ff6964879b81b22cdedf6d6b8280806075
hazardno-dot <hazardno@gmail.com>
2026-09-04 18:26:50 +0200

    Route new product apply through preview updater

--- control-center/api/create-new-product.js
+++ control-center/api/create-new-product.js
@@ -1,4 +1,4 @@
 module.exports = async function handler(req, res) {
-  const mod = await import("../lib/create-new-product-engine.mjs");
+  const mod = await import("../lib/create-new-product-preview-engine.mjs");
   return mod.default(req, res);
 };
