5b72c906268c4c39d1426499abebd47ad8789ece
hazardno-dot <hazardno@gmail.com>
2026-09-09 03:00:40 +0200

    Mount desktop Discover catalog

--- playnice-site/src/index.js
+++ playnice-site/src/index.js
@@ -9,6 +9,7 @@ import MobilePrivateSelectionProfile from "./MobilePrivateSelectionProfile";
 import MobileShopV2 from "./MobileShopV2";
 import MobileShopReveal from "./MobileShopReveal";
 import MobileMenuContact from "./MobileMenuContact";
+import DesktopDiscoverCatalog from "./DesktopDiscoverCatalog";
 import CartInteractionV2 from "./CartInteractionV2";
 import DiscoveryImageFallback from "./DiscoveryImageFallback";
 import FragranceIntelligenceToneEnhancer from "./FragranceIntelligenceToneEnhancer";
@@ -57,6 +58,7 @@ root.render(
     <MobileShopV2 />
     <MobileShopReveal />
     <MobileMenuContact />
+    <DesktopDiscoverCatalog />
     <CartInteractionV2 />
     <DiscoveryImageFallback />
     <FragranceIntelligenceToneEnhancer />
