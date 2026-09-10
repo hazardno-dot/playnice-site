ce2dbe2ddca4af6a5a7521a82139265b1e5b713c
hazardno-dot <hazardno@gmail.com>
2026-09-10 03:14:42 +0200

    Load pager composition fixes after mobile modal styles

--- playnice-site/src/index.js
+++ playnice-site/src/index.js
@@ -35,6 +35,7 @@ import "./MobileClosingFooterV2.css";
 import "./MobileShopV2.css";
 import "./MobileShopReveal.css";
 import "./MobileProductModalV2.css";
+import "./MobileProductModalPagerFix.css";
 import "./ProductModalPurchaseV2.css";
 import "./MobileCartV2.css";
 import "./MobileCheckoutV2.css";
