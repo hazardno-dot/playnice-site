7f972f330cc974a05df27ff9f1781b2c2c050285
hazardno-dot <hazardno@gmail.com>
2026-09-10 15:10:19 +0200

    Rename mobile pager polish stylesheet

--- playnice-site/src/index.js
+++ playnice-site/src/index.js
@@ -35,7 +35,7 @@ import "./mobile-v2/navigation/MobileClosingFooterV2.css";
 import "./mobile-v2/shop/MobileShopV2.css";
 import "./mobile-v2/shop/MobileShopReveal.css";
 import "./mobile-v2/modal/MobileProductModalV2.css";
-import "./mobile-v2/modal/MobileProductModalPagerFix.css";
+import "./mobile-v2/modal/MobileProductModalPagerPolish.css";
 import "./ProductModalPurchaseV2.css";
 import "./mobile-v2/cart-checkout/MobileCartV2.css";
 import "./mobile-v2/cart-checkout/MobileCheckoutV2.css";
