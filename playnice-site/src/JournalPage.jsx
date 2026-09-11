88edbfbc8c0abad735190fed88be9a10081d56fb
hazardno-dot <hazardno@gmail.com>
2026-09-11 16:54:27 +0200

    Use shared Journal slug helper

--- playnice-site/src/JournalPage.jsx
+++ playnice-site/src/JournalPage.jsx
@@ -1,39 +1,9 @@
 import React, { useEffect, useMemo, useState } from "react";
 import "./JournalPage.css";
 import "./JournalProgressiveReveal.css";
+import { getJournalArticleSlug, getJournalText } from "./lib/journalSlug";
 
 const MOBILE_ARCHIVE_BATCH = 5;
-
-const getJournalText = (field, lang = "sr") => {
-  if (!field) return "";
-
-  if (typeof field === "string") return field;
-
-  if (typeof field === "object") {
-    return field[lang] || field.en || field.sr || "";
-  }
-
-  return "";
-};
-
-const getJournalArticleSlug = (article) => {
-  if (!article) return "";
-
-  const title =
-    getJournalText(article.title, "en") ||
-    getJournalText(article.title, "sr") ||
-    `article-${article.id}`;
-
-  const normalizedTitle = String(title)
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/&/g, "and")
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/^-+|-+$/g, "");
-
-  return `${article.id}-${normalizedTitle}`;
-};
 
 function JournalPage({
   lang = "sr",
