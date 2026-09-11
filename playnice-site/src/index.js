aaa2256d885e36c40060e9eaa842b3fbed6f62ba
hazardno-dot <hazardno@gmail.com>
2026-09-12 00:08:47 +0200

    Register clean mobile PDP styles

--- playnice-site/src/index.js
+++ playnice-site/src/index.js
@@ -34,6 +34,7 @@ import "./mobile-v2/modal/MobileProductModalBase.css";
 import "./mobile-v2/modal/MobileProductModalPagerCore.css";
 import "./mobile-v2/modal/MobileProductModalPagerPolish.css";
 import "./ProductModalPurchaseV2.css";
+import "./mobile-v2/product-page/MobileProductPage.css";
 import "./mobile-v2/cart-checkout/MobileCartV2.css";
 import "./mobile-v2/cart-checkout/MobileCheckoutV2.css";
 import "./mobile-v2/cart-checkout/MobileStickyCtaV2.css";
