c2acff037b870d4b216d84bff2a54e5d98f65d79
hazardno-dot <hazardno@gmail.com>
2026-09-12 18:29:39 +0200

    Mount CC product editorial contexts

--- control-center/src/ControlCenterManagers.jsx
+++ control-center/src/ControlCenterManagers.jsx
@@ -6,6 +6,7 @@ import ProductBulkPasteBridge from "./ProductBulkPasteBridge";
 import ProductMediaUploadBridge from "./ProductMediaUploadBridge";
 import ProductMediaStatusBridge from "./ProductMediaStatusBridge";
 import DiscoveryBulkPasteBridge from "./DiscoveryBulkPasteBridge";
+import ProductEditorialContextsBridge from "./ProductEditorialContextsBridge";
 import ControlledApplyManager from "./ControlledApplyManager";
 import ProductWorkflowBridge from "./ProductWorkflowBridge";
 import ProductWorkflowAdvanceBridge from "./ProductWorkflowAdvanceBridge";
@@ -103,6 +104,7 @@ export default function ControlCenterManagers() {
     <ProductMediaStatusBridge />
     <ProductBulkPasteBridge />
     <DiscoveryBulkPasteBridge />
+    <ProductEditorialContextsBridge />
     <ProductWorkflowBridge />
     <ProductWorkflowAdvanceBridge />
     <ProductCatalogCountBridge />
