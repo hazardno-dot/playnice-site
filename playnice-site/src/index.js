22a46a7b8e4a4e3886b7923555241712f17e6a3b
hazardno-dot <hazardno@gmail.com>
2026-09-15 00:53:24 +0200

    Load final card copy before storefront render

--- playnice-site/src/index.js
+++ playnice-site/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./data/products/applyCardCopyFinalOverrides";
 import "./index.css";
 import App from "./App";
 import PrivateSelectionEnhancer from "./PrivateSelectionEnhancer";
