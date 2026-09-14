f77bb2e2db493632c1f9c8ce84033307cc0ddaab
hazardno-dot <hazardno@gmail.com>
2026-09-15 01:46:19 +0200

    Load product page accordion chevron polish

--- playnice-site/src/index.js
+++ playnice-site/src/index.js
@@ -38,6 +38,7 @@ import "./ProductModalPurchaseV2.css";
 import "./ProductImageNormalization.css";
 import "./mobile-v2/product-page/MobileProductPage.css";
 import "./mobile-v2/product-page/MobileProductPageBadgePolish.css";
+import "./mobile-v2/product-page/MobileProductPageAccordionChevrons.css";
 import "./mobile-v2/cart-checkout/MobileCartV2.css";
 import "./mobile-v2/cart-checkout/MobileCheckoutV2.css";
 import "./mobile-v2/cart-checkout/MobileStickyCtaV2.css";
