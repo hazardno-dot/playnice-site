a3f6a16e7ae0406d42f5287c0489f1fb8d696b16
hazardno-dot <hazardno@gmail.com>
2026-09-14 02:23:41 +0200

    Control Center apply: riiffs-freeze-extrait (sizes)

--- playnice-site/src/data/products/index.js
+++ playnice-site/src/data/products/index.js
@@ -1777,7 +1777,7 @@ export const products = [
     shortName: "RiiFFS Freeze",
     category: "Arabian",
     image: "/products/riiffs-freeze-extrait.png",
-    sizes: { "5ml": 4.5, "10ml": 8, "20ml": 15 },
+    sizes: { "5ml": 5, "10ml": 9, "20ml": 17 },
     badge: "MOST WANTED",
     rating: 8.3,
     ratingLabel: "Fresh Favorite",
