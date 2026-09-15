7662cd40be25c91cb3d19ec423305e2d019c7259
hazardno-dot <hazardno@gmail.com>
2026-09-15 23:50:56 +0200

    Mount Social storage diagnostic

--- control-center/src/ControlCenterManagers.jsx
+++ control-center/src/ControlCenterManagers.jsx
@@ -29,6 +29,7 @@ import ExhibitionManager from "./ExhibitionManager";
 import SocialManager from "./SocialManager";
 import SocialMediaOverrideBridge from "./SocialMediaOverrideBridge";
 import SocialReadinessBridge from "./SocialReadinessBridge";
+import SocialStorageDiagnosticBridge from "./SocialStorageDiagnosticBridge";
 import SocialInstagramTestPublishBridge from "./SocialInstagramTestPublishBridge";
 import SocialFacebookTestPublishBridge from "./SocialFacebookTestPublishBridge";
 import SocialInstagramStoryTestPublishBridge from "./SocialInstagramStoryTestPublishBridge";
@@ -122,6 +123,7 @@ export default function ControlCenterManagers() {
     <SocialManager />
     <SocialMediaOverrideBridge />
     <SocialReadinessBridge />
+    <SocialStorageDiagnosticBridge />
     <SocialInstagramTestPublishBridge />
     <SocialFacebookTestPublishBridge />
     <SocialInstagramStoryTestPublishBridge />
