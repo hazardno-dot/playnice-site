f58797fba931e8f2d6c7276749376756de677515
hazardno-dot <hazardno@gmail.com>
2026-09-18 00:23:45 +0200

    Control Center Announcement apply: thomas-kosmala-no4-announcement

--- playnice-site/src/data/announcementConfig.generated.js
+++ playnice-site/src/data/announcementConfig.generated.js
@@ -3,8 +3,8 @@ export const ANNOUNCEMENT_ITEMS = [
     id: "thomas-kosmala-no4-announcement",
     enabled: true,
     text: {
-      sr: "✦ NOVO: Thomas Kosmala No. 4 Après l'Amour 10ml + Mystery Designer Sample • Limited Stock!",
-      en: "✦ NEW: Thomas Kosmala No. 4 Après l'Amour 10ml + Mystery Designer Sample • Limited Stock",
+      sr: "✦ NOVO: Thomas Kosmala No. 4 Après l'Amour 10ml + Mystery Sample • Limited Stock!",
+      en: "✦ NEW: Thomas Kosmala No. 4 Après l'Amour 10ml + Mystery Sample • Limited Stock!",
     },
     icon: "→",
     tone: "new-shop",
