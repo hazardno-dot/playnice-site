8716d618e8f81d34f37ea81c8a190b3da2eb0039
hazardno-dot <hazardno@gmail.com>
2026-09-20 01:34:17 +0200

    Shorten mobile PDP cart feedback

--- playnice-site/src/mobile-v2/product-page/MobileProductPage.jsx
+++ playnice-site/src/mobile-v2/product-page/MobileProductPage.jsx
@@ -226,7 +226,7 @@ export default function MobileProductPage({
     addedCartTimeoutRef.current = window.setTimeout(() => {
       setAddedCartKey("");
       addedCartTimeoutRef.current = null;
-    }, 1300);
+    }, 600);
   };
 
   return (
