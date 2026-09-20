4225c886421d1f68e25c9c3d319d05a5728ea7c6
hazardno-dot <hazardno@gmail.com>
2026-09-20 13:58:14 +0200

    refactor: point App header import to feature folder

--- playnice-site/src/App.js
+++ playnice-site/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useLayoutEffect, useMemo, useRef, useState, useCallback } from "react";
 import "./App.css";
-import HeaderNext from "./HeaderNext";
+import HeaderNext from "./features/header/HeaderNext";
 import { trackPageView, trackEvent, trackMeta } from "./lib/ga";
 import { journalArticles } from "./data/journal";
 import { categoryLabels, products } from "./data/products";
