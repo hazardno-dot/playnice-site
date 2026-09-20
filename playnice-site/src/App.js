09dcd617b1f52b20feaf1478028b96230f49e453
hazardno-dot <hazardno@gmail.com>
2026-09-20 15:30:42 +0200

    refactor: remove final legacy modal visibility reference

--- playnice-site/src/App.js
+++ playnice-site/src/App.js
@@ -6527,11 +6527,7 @@ const DeliveryReturnsMini = ({ surface = "footer" }) => {
 </div>
 
 {newArrivalProducts.length > 0 && (
-  <div
-    className={`new-arrivals-marquee ${
-      productModalVisible ? "is-product-modal-open" : ""
-    }`}
-  >
+  <div className="new-arrivals-marquee">
     <div className="new-arrivals-track">
       {[false, true].map((isClone, groupIndex) => (
         <div
