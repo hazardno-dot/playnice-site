cde507b1a96821a1f52961512d06326fd4d27ca8
hazardno-dot <hazardno@gmail.com>
2026-09-20 21:04:36 +0200

    Bind What To Wear copy to canonical product order

--- playnice-site/src/data/products/productWhatToWearContext.js
+++ playnice-site/src/data/products/productWhatToWearContext.js
@@ -1,11 +1,11 @@
-import { productWearContext } from "./productWearContext";
+import { products } from "./index";
 import part1 from "./productWhatToWearContext.part1";
 import part2 from "./productWhatToWearContext.part2";
 import part3 from "./productWhatToWearContext.part3";
 import part4 from "./productWhatToWearContext.part4";
 
 const copy = [...part1, ...part2, ...part3, ...part4];
-const productNames = Object.keys(productWearContext);
+const productNames = products.map((product) => product.name);
 
 export const productWhatToWearContext = productNames.reduce((result, productName, index) => {
   const entry = copy[index];
