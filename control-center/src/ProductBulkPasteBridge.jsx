bf9578d59cbd99cb5f5791c91ff7a334b1fef37b
hazardno-dot <hazardno@gmail.com>
2026-09-20 20:42:16 +0200

    Fix Product bulk Note Map raw import

--- control-center/src/ProductBulkPasteBridge.jsx
+++ control-center/src/ProductBulkPasteBridge.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import { createPortal } from "react-dom";
 import { products } from "@shop/data/products/index.js";
 import { discoveryProfiles } from "@shop/data/products/discoveryProfiles";
-import noteMapSource from "@shop/TheNoteMap.jsx?raw";
+import noteMapSource from "@shop/features/note-map/TheNoteMapImpl.jsx?raw";
 import { validateInlineFields } from "./inlineValidationRules.mjs";
 import "./product-bulk-paste.css";
 
